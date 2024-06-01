568466599c8dcbc6fd6ff64ef4a4583ee46de7ff
ckotonew <ckotonew@gmail.com>
2024-06-01 20:11:13 -0300

    inclusao de router nas paginas

--- src/app/componentes/contato/contato.component.ts
+++ src/app/componentes/contato/contato.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
